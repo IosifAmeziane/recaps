3b15665180b54aaa57df63e4bab5bd5e99134a3e
iosif ameziane <iosif.ameziane@gmail.com>
2020-11-16 21:31:22 +0200

    secondClass reference in firstClass

--- src/main/java/git/FirstClass.java
+++ src/main/java/git/FirstClass.java
@@ -3,5 +3,8 @@ package git;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("First class");
+
+        SecondClass secondClass = new SecondClass();
+        secondClass.sayHello();
     }
 }
