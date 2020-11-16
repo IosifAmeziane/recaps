07ee9273e0f0a75e99184ef85778d7f6e6a81486
iosif ameziane <iosif.ameziane@gmail.com>
2020-11-16 21:27:46 +0200

    dublicate print first class

--- src/main/java/git/FirstClass.java
+++ src/main/java/git/FirstClass.java
@@ -3,5 +3,6 @@ package git;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("First class");
+        System.out.println("First class");
     }
 }
